15312ebc4e7cb79fa610d64ab3d509bd6cafb3b8
Patryk Gliszczyński <patryk.gliszczynski94@gmail.com>
2016-05-24 21:19:36 +0200

    Dodano warunki sprawdzajace parametry

--- run.sh
+++ run.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-mpirun -n $1 ./therms $2 $3
+numbers='^[0-9]+$'
+
+if [ "$#" -ne 3 ]; then
+	echo "[ERROR]: Illegal number of parameters"
+elif ! [[ $1 =~ $numbers ]]; then
+	echo "[ERROR]: Parametr 1 is not a positive, real number"
+elif ! [[ $2 =~ $numbers ]]; then
+	echo "[ERROR]: Parametr 2 is not a positive, real number"
+elif ! [[ $3 =~ $numbers ]]; then
+	echo "[ERROR]: Parametr 3 is not a positive, real number"
+elif ! [[ $1 -gt 0 ]]; then
+	echo "ERROR]: Number of people must be greater than 0"
+elif ! [[ $2 -gt 0 ]]; then
+	echo "ERROR]: Number of locker rooms must be greater than 0"
+elif ! [[ $3 -gt 0 ]]; then 
+	echo "ERROR]: Number of lockers must be greater than 0"
+elif ! [[ $1 -gt $(($2 * $3)) ]]; then
+	echo "[ERROR]: Number of people is not large enough"
+else
+	mpirun -n $1 ./therms $2 $3
+fi
